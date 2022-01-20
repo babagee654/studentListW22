f2177528058a61706b0d313059b7c6d028cb4049
babagee654 <75646125+babagee654@users.noreply.github.com>
2022-01-20 15:45:27 -0500

    Added address

--- src/studentlist/Student.java
+++ src/studentlist/Student.java
@@ -8,6 +8,8 @@ package studentlist;
 public class Student {
 
     private String name;
+    
+    private String address;
 
     public Student(String name) {
         this.name = name;
@@ -20,5 +22,16 @@ public class Student {
     public void setName(String name) {
         this.name = name;
     }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+
     
+
 }
